95596815f1a4d4b18dd4c3311a46bdbf799a13ae
Madan Gurrala <mgurrala9086@conestogac.on.ca>
2019-08-07 04:43:39 -0400

    Populating Employee details and Database created with all the required tables

--- app/src/main/java/com/example/hrincidentreporting/DataBaseHandler.java
+++ app/src/main/java/com/example/hrincidentreporting/DataBaseHandler.java
@@ -1,6 +1,8 @@
 package com.example.hrincidentreporting;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,6 +11,9 @@ public class DataBaseHandler extends SQLiteOpenHelper {
 
     public static final String databaseName = "HrRecords.db";
     public static final String tableName = "tbl_IncidentHistory";
+    public static final String employeeTable = "tbl_Employee";
+    public static final String bodyPartsTable = "tbl_BodyParts";
+    public  static final String employeeId = "_id";
     public static final String title = "Title";
     public static final String incidentId = "ID";
     public static final String incidentDate = "IncidentDate";
@@ -24,7 +29,6 @@ public class DataBaseHandler extends SQLiteOpenHelper {
     public  DataBaseHandler(Context context){
 
         super(context, databaseName, null, 1 );
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
 
     }
 
@@ -42,4 +46,61 @@ public class DataBaseHandler extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
 
     }
+
+    //Method to insert records into database which takes FirstName, LastName and Marks
+    public boolean insertRecord(String Title, String IncidentDate, String EmployeeNumber,
+                                String EmployeeName, String Gender, String Shift, String Department,
+                                String Position, String IncidentType, String InjuredBodyPart  ){
+        //Instantiating SQLlitedatabase to get Writable database
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        //Instantiating Content values to add parameters passed with method to use in the query
+        ContentValues cValues = new ContentValues();
+        cValues.put(title, Title );
+        cValues.put(incidentDate,IncidentDate );
+        cValues.put(empNumber, EmployeeNumber);
+        cValues.put(empName, EmployeeName);
+        cValues.put(gender, Gender);
+        cValues.put(shift, Shift);
+        cValues.put(department, Department);
+        cValues.put(position, Position);
+        cValues.put(incidentType, IncidentType);
+        cValues.put(bodyPart, InjuredBodyPart);
+
+
+        //storing the boolean return of insert command in response variable
+        long response =  sqLiteDatabase.insert(tableName,null, cValues);
+
+        //using response variable to return either true or false
+        if(response == -1){
+            return  false;
+        }else{
+            return true;
+        }
+    }
+
+    //Method to view records (Using Cursor type since raw query returns Cursor)
+    public Cursor viewRecords(){
+        //Instantiating SQLiteDatabase to get the writable Database
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor response = sqLiteDatabase.rawQuery("select * from "+tableName, null);
+        return  response;
+
+    }
+
+    //Method to view one record which returns cursor
+    public Cursor viewEmployeeRecord(String id){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        //SQl query to retrieve a record based on the given ID(taken through parameter)
+        Cursor response = sqLiteDatabase.rawQuery("SELECT * FROM " + employeeTable +" WHERE "+ employeeId + " = " + id,null, null);
+        return  response;
+    }
+
+    public Cursor selectBodyParts(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        //SQl query to retrieve a record based on the given ID(taken through parameter)
+        Cursor response = sqLiteDatabase.rawQuery("SELECT * FROM " + bodyPartsTable, null, null);
+        return  response;
+    }
+
 }
--- app/src/main/java/com/example/hrincidentreporting/ReportIncident.java
+++ app/src/main/java/com/example/hrincidentreporting/ReportIncident.java
@@ -1,14 +1,18 @@
 package com.example.hrincidentreporting;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,35 +20,57 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Date;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
 
 
 public class ReportIncident extends Fragment {
 
-    TextView labelTitle, labelIncident, labelEmpId, labelDate,
+    TextView labelTitle, labelIncident, labelEmpId, labelEmpName, labelDate,
             labelGender, labelShift, labelDept, labelPosition,
             labelIncType, labelInjPart;
-    EditText title, incidentId, empId, date, department, position;
+    EditText title, incidentId, empId, empName, date, department, position;
     RadioGroup genderGroup;
     RadioButton male, female;
     private Spinner shift, incidentType, InjBodyPart;
     private Button submitButton;
 
-     private String[] shiftArray, incidentArray;
+     private String[] shiftArray, incidentArray, bodyParts;
+     private int[] spinnerBParts;
+    DataBaseHandler myDb;
+    View view;
+    String genderStr, shiftStr, incidentStr, injuredBdyStr;
+
+
+    public boolean validateId(){
+        /*This block of code checks if something is entered in ID field and then try to parses to int,
+        if not parsable sets an error to ID filed.*/
+            if(empId.getText().length() == 0){
+                empId.setError("Enter Employee ID");
+                return false;
+            }else {
+                empId.setError(null);
+                return true;
+            }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_report_incident, container, false);
+       view = inflater.inflate(R.layout.fragment_report_incident, container, false);
 
-        DataBaseHandler myDb = new DataBaseHandler(getContext());
+        myDb = new DataBaseHandler(getContext());
 
         labelTitle = (TextView) view.findViewById(R.id.titleLabel);
         labelIncident = (TextView) view.findViewById(R.id.labelIncidentId);
         labelEmpId = (TextView) view.findViewById(R.id.labelEmpId);
+        labelEmpName = (TextView) view.findViewById(R.id.labelEmpName);
         labelDate = (TextView) view.findViewById(R.id.labelDateId);
         labelGender = (TextView) view.findViewById(R.id.labelGenderId);
         labelShift = (TextView) view.findViewById(R.id.labelShiftId);
@@ -56,6 +82,7 @@ public class ReportIncident extends Fragment {
         title = (EditText) view.findViewById(R.id.titleId);
         incidentId = (EditText) view.findViewById(R.id.incidentId);
         empId = (EditText) view.findViewById(R.id.empId);
+        empName = (EditText) view.findViewById(R.id.empName);
         date = (EditText) view.findViewById(R.id.dateId);
         department = (EditText) view.findViewById(R.id.departmentId);
         position = (EditText) view.findViewById(R.id.positionId);
@@ -70,6 +97,10 @@ public class ReportIncident extends Fragment {
 
         submitButton = (Button) view.findViewById(R.id.buttonId);
 
+        String currentDate = new SimpleDateFormat("MM/dd/YYYY", Locale.getDefault()).format(new Date());
+
+        date.setText(currentDate);
+
         shiftArray = new String[]{"Shift A", "Shift B", "Shift C"};
         incidentArray = new String[]{"Near Miss", "First Aid", "Medical Aid"};
 
@@ -81,10 +112,36 @@ public class ReportIncident extends Fragment {
                 android.R.layout.simple_spinner_dropdown_item, incidentArray);
         incidentType.setAdapter(adptIncident);
 
+        String bodyParts[] = new String[] {"Body_Part"};
+        int spinnerBParts[] = new int[] {android.R.id.text1};
 
+        Cursor cursor = myDb.selectBodyParts();
+
+        SimpleCursorAdapter adapterBodyParts = new SimpleCursorAdapter(view.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, cursor, bodyParts, spinnerBParts);
+
+        adapterBodyParts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        InjBodyPart.setAdapter(adapterBodyParts);
         reportIncident();
-
         return view;
+    }
+
+    public boolean genderSelection(){
+        boolean ischecked = false;
+        if(male.isChecked()){
+            genderStr = male.getText().toString();
+            ischecked = true;
+        }
+        if(female.isChecked()){
+            genderStr = female.getText().toString();
+            ischecked = true;
+        }
+        if(!male.isChecked() && !female.isChecked()){
+
+            ischecked = false;
+        }
+
+        return ischecked;
     }
 
     public void reportIncident(){
@@ -92,9 +149,65 @@ public class ReportIncident extends Fragment {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String titleStr, incidentDateStr, empNumberStr, empNameStr, shiftStr, departmentStr,
+                        positionStr, incidentTypeStr, bodyPartStr;
+                titleStr = title.getText().toString();
+                incidentDateStr = date.getText().toString();
+                empNumberStr = empId.getText().toString();
+
+
+                if(getEmployeeDetails()) {
+
+                   /* boolean createdRecord =myDb.insertRecord()*/
+
+
+
+                }
+
                 Toast.makeText(getActivity(), "Hello ", Toast.LENGTH_LONG ).show();
             }
         });
     }
+
+    public boolean getEmployeeDetails(){
+
+        if(validateId()){
+            Cursor cursor =  myDb.viewEmployeeRecord(empId.getText().toString().trim());
+
+            if(cursor.getCount() == 0){
+
+                showData("Error", "Entered Employee ID doesn't exist");
+                return false;
+            }else{
+                StringBuffer buffer = new StringBuffer();
+                //Passing all the Cursor records to Buffer using index
+                while (cursor.moveToNext()){
+                    empName.setText(cursor.getString(1));
+                    department.setText(cursor.getString(2));
+                    position.setText(cursor.getString(3));
+                }
+                cursor.close();
+                return true;
+            }
+        }else{
+
+            return false;
+        }
+    }
+
+    /*This method takes two string parameters and passes it to AlertDialog builder to display the data passed to buffer
+      everytime when this function is called*/
+    public void showData(String title, String message){
+        //Instantiating AlertDialog builder
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+        //Setting the dialog builder can be cancellable
+        alertBuilder.setCancelable(true);
+        //Passing taken parameters to setTitle & setMessage
+        alertBuilder.setTitle(title);
+        alertBuilder.setMessage(message);
+        alertBuilder.show();
+    }
+
 
 }
